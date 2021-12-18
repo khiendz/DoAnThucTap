0fc7836632e668cb1f9796a226a956df4c293954
LAPTOP-DPS9GSGG\admin <637640@sv.vnua.edu.vn>
2021-12-19 01:20:09 +0700

    update routing

--- DoAnThucTap/src/app/app-routing.module.ts
+++ DoAnThucTap/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginFormComponent, ResetPasswordFormComponent, CreateAccountFormComponent, ChangePasswordFormComponent } from './shared/components';
 import { AuthGuardService } from './shared/services';
+import { AuthGuard } from './../app/_helpers/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { TasksComponent } from './pages/tasks/tasks.component';
@@ -11,72 +12,68 @@ import { RoleManagerComponent } from './pages/role-manager/role-manager.componen
 import { DepartmentManagerComponent } from './pages/department-manager/department-manager.component';
 import { ContractComponent } from './pages/contract/contract.component';
 import { TimekeepingComponent } from './pages/timekeeping/timekeeping.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   {
     path: 'tasks',
     component: TasksComponent,
-    canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'profile',
     component: ProfileComponent,
-    canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'home',
     component: HomeComponent,
-    canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'employee',
     component: EmployeeManagerComponent,
-    canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'contract',
     component: ContractComponent,
-    canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'role',
     component: RoleManagerComponent,
-    canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'department',
     component: DepartmentManagerComponent,
-    canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'login-form',
     component: LoginFormComponent,
-    canActivate: [ AuthGuardService ]
   },
   {
     path: 'reset-password',
     component: ResetPasswordFormComponent,
-    canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'create-account',
     component: CreateAccountFormComponent,
-    canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'change-password/:recoveryCode',
     component: ChangePasswordFormComponent,
-    canActivate: [ AuthGuardService ]
+    canActivate: [ AuthGuard ]
   },
   {
     path: 'timekeeping',
     component: TimekeepingComponent,
-    canActivate: [ AuthGuardService ]
-  },
-  {
-    path: '**',
-    redirectTo: 'home'
+    canActivate: [ AuthGuard ]
   }
 ];
 
